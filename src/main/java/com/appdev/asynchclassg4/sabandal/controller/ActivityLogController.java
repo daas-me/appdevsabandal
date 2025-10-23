4884cd97a81d9108a861bb19ad908ebf64e8b378
daas-me <110253987+daas-me@users.noreply.github.com>
2025-10-23 14:32:16 +0800

    add crud for controller

--- src/main/java/com/appdev/asynchclassg4/sabandal/controller/ActivityLogController.java
+++ src/main/java/com/appdev/asynchclassg4/sabandal/controller/ActivityLogController.java
@@ -3,11 +3,15 @@ package com.appdev.asynchclassg4.sabandal.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.appdev.asynchclassg4.sabandal.entity.ActivityLogEntity;
@@ -29,4 +33,13 @@ public class ActivityLogController {
         return actlogserv.getAllActivityLog();
     }
 
+    @PutMapping("/updateActivityLog")
+    public ActivityLogEntity updateActivityLog(@RequestParam int log_id,@RequestBody ActivityLogEntity newActivityLog){
+        return actlogserv.updateActivityLog(log_id, newActivityLog);
+    }
+
+    @DeleteMapping("/deleteActivityLog")    
+    public String deleteActivityLog(@PathVariable int log_id){
+        return actlogserv.deleteActivityLog(log_id);
+    }
 }
--- src/main/java/com/appdev/asynchclassg4/sabandal/controller/FolderController.java
+++ src/main/java/com/appdev/asynchclassg4/sabandal/controller/FolderController.java
@@ -2,9 +2,14 @@ package com.appdev.asynchclassg4.sabandal.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,19 +20,31 @@ import com.appdev.asynchclassg4.sabandal.service.FolderService;
 @RestController
 @RequestMapping(method = RequestMethod.GET,path="/api/folders")
 public class FolderController {
-    private final FolderService folderService;
+    
+    @Autowired
+    FolderService folderserv;
 
     public FolderController(FolderService folderService){
-        this.folderService = folderService;
+        this.folderserv = folderService;
     }
 
     @GetMapping
     public List<FolderEntity> getAllFolder(){
-        return folderService.getAllFolder();
+        return folderserv.getAllFolder();
     }
 
     @PostMapping
     public FolderEntity addFolder(@RequestBody FolderEntity folder){
-        return folderService.saveFolder(folder);
+        return folderserv.saveFolder(folder);
+    }
+
+    @PutMapping("/updateFolder")
+    public FolderEntity updateFolder(@RequestHeader int folder_id, @RequestBody FolderEntity folder){
+        return folderserv.updateFolder(folder_id, folder);
+    }
+
+    @DeleteMapping("/deleteFolder")
+    public String deleteFolder(@PathVariable int folder_id){
+        return folderserv.deleteFolder(folder_id);
     }
 }
